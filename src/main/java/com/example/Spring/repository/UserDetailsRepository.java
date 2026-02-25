a81857efec788ea2137390596d03f0d0c3a82585
Sunil <sunilmallela9177@gmail.com>
2026-02-25 12:45:56 +0530

    first

---
+++ src/main/java/com/example/Spring/repository/UserDetailsRepository.java
@@ -0,0 +1,12 @@
+package com.example.Spring.repository;
+
+import com.example.Spring.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserDetailsRepository extends JpaRepository<Users,Long> {
+    Optional<Users> findByUsername(String username);
+}
